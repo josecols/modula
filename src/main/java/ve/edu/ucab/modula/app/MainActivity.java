16fb235de2fb3f23f8724555d392f18b1d92906e
josecols <josecolsg@gmail.com>
2014-04-10 11:29:45 -0430

    Agregados botón de frases y nueva conversación al MainActivity

--- src/main/java/ve/edu/ucab/modula/app/MainActivity.java
+++ src/main/java/ve/edu/ucab/modula/app/MainActivity.java
@@ -13,10 +13,9 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -28,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_ajustes) {
             return true;
         }
         return super.onOptionsItemSelected(item);
