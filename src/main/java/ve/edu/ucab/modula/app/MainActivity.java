7f49b77a13ac14aab15cc4d28db7fe5b2f628fd8
josecols <josecolsg@gmail.com>
2014-04-11 18:39:18 -0430

    Agregada vista de frases Además se cambió el icono de favorito y se renombraron rutas eliminando espacios

--- src/main/java/ve/edu/ucab/modula/app/MainActivity.java
+++ src/main/java/ve/edu/ucab/modula/app/MainActivity.java
@@ -1,5 +1,6 @@
 package ve.edu.ucab.modula.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,25 +11,25 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_frases);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_ajustes) {
-            return true;
+        switch (id) {
+            case R.id.action_ajustes:
+                return true;
+            case R.id.action_frases:
+                Intent intent = new Intent(this, FrasesActivity.class);
+                startActivity(intent);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
