57455db4d12a32c00c93dbcb85d1ea42fd7d1dfd
josecols <josecolsg@gmail.com>
2014-04-11 20:19:48 -0430

    Agregado menú contextual a FrasesActivity

--- src/main/java/ve/edu/ucab/modula/app/FrasesActivity.java
+++ src/main/java/ve/edu/ucab/modula/app/FrasesActivity.java
@@ -2,38 +2,81 @@ package ve.edu.ucab.modula.app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * Created by Jose Cols on 10/04/14.
- */
 public class FrasesActivity extends ActionBarActivity {
+
+    private ListView vistaFrases;
+    private ActionMode mActionMode;
+    private final String[] elementos = {"Elemento 1", "Elemento 2", "Elemento 3"};
+
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.frases_context, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_nuevafrase:
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_frases);
+        this.vistaFrases = (ListView) findViewById(R.id.vista_frases);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.elementos);
+        this.vistaFrases.setAdapter(adapter);
+
+        this.vistaFrases.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mActionMode != null)
+                    return false;
+                mActionMode = startActionMode(mActionModeCallback);
+                view.setSelected(true);
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.frases, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_nuevafrase) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
